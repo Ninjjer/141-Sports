b71a4ff984503df32e0fb0ef2deaffa06e51a00a
Gustavo Thiago Ribeiro Farina <161526981+GustavoHransa@users.noreply.github.com>
2024-10-12 13:36:40 -0300

    Falta header e footer
    mais tá ai uns 75% feito

--- favoritos/favoritos.php
+++ favoritos/favoritos.php
@@ -11,7 +11,7 @@
 <section class="main">
         
         <div class="card card">
-                <img src="./favoritos/camisetas/alemanha.png" alt="Camiseta Alemanha">
+                <img src="camisetas/alemanha.png" alt="Camiseta Alemanha">
                 <h1>Camiseta Alemanha - Rose</h1>
                 <p class="preco">R$ 159,90</p>
                 <button class="comprar">Comprar</button>
@@ -19,7 +19,7 @@
                 <button class="excluir">Excluir</button>
         </div>
         <div class="card card2">
-                <img src="./favoritos/camisetas/flamengo.png" alt="Camiseta Alemanha">
+                <img src="camisetas/flamengo.png" alt="Camiseta Alemanha">
                 <h1>Camiseta Flamengo</h1>
                 <p class="preco2">R$ 159,90</p>
                 <button class="comprar2">Comprar</button>
@@ -27,7 +27,7 @@
                 <button class="excluir2">Excluir</button>
         </div>
         <div class="card card3">
-                <img src="./favoritos/camisetas/bahia.png" alt="Camiseta Alemanha">
+                <img src="camisetas/bahia.png" alt="Camiseta Alemanha">
                 <h1>Camiseta Bahia</h1>
                 <p class ="preco3">R$ 159,90</p>
                 <button class="comprar3">Comprar</button>
@@ -36,27 +36,55 @@
         </div>
     </section>
     <section class="main">
-        <div class="card card1">
-                <img src="./favoritos/camisetas/alemanha.png" alt="Camiseta Alemanha">
+        <div class="card card4">
+                <img src="camisetas/alemanha.png" alt="Camiseta Alemanha">
                 <h1>Camiseta Alemanha - Rose</h1>
-                <p>R$ 159,90</p>
+                <p class ="preco4">R$ 159,90</p>
                 <button class="comprar4">Comprar</button>
-                
-                <button>Excluir</button>
+                <button class="carrinho4">+ Carrinho</button>
+                <button class="excluir4">Excluir</button>
         </div>
-        <div class="card card2">
-                <img src="./favoritos/camisetas/flamengo.png" alt="Camiseta Alemanha">
+        <div class="card card5">
+                <img src="camisetas/flamengo.png" alt="Camiseta Alemanha">
                 <h1>Camiseta Flamengo</h1>
-                <p>R$ 159,90</p>
+                <p class ="preco5">R$ 159,90</p>
                 <button class="comprar5">Comprar</button>
-                <button>Excluir</button>
+                <button class="carrinho5">+ Carrinho</button>
+                <button class="excluir5">Excluir</button>
         </div>
-        <div class="card card3">
-                <img src="./favoritos/camisetas/bahia.png" alt="Camiseta Alemanha">
+        <div class="card card6">
+                <img src="camisetas/bahia.png" alt="Camiseta Alemanha">
                 <h1>Camiseta Bahia</h1>
-                <p>R$ 159,90</p>
+                <p class ="preco6">R$ 159,90</p>
                 <button class="comprar6">Comprar</button>
-                <button>Excluir</button>
+                <button class="carrinho6">+ Carrinho</button>
+                <button class="excluir6">Excluir</button>
+        </div>
+    </section>
+    <section class="main">
+        <div class="card card7">
+                <img src="camisetas/alemanha.png" alt="Camiseta Alemanha">
+                <h1>Camiseta Alemanha - Rose</h1>
+                <p class ="preco7">R$ 159,90</p>
+                <button class="comprar7">Comprar</button>
+                <button class="carrinho7">+ Carrinho</button>
+                <button class="excluir7">Excluir</button>
+        </div>
+        <div class="card card8">
+                <img src="camisetas/flamengo.png" alt="Camiseta Alemanha">
+                <h1>Camiseta Flamengo</h1>
+                <p class ="preco8">R$ 159,90</p>
+                <button class="comprar8">Comprar</button>
+                <button class="carrinho8">+ Carrinho</button>
+                <button class="excluir8">Excluir</button>
+        </div>
+        <div class="card card9">
+                <img src="camisetas/bahia.png" alt="Camiseta Alemanha">
+                <h1>Camiseta Bahia</h1>
+                <p class ="preco9">R$ 159,90</p>
+                <button class="comprar9">Comprar</button>
+                <button class="carrinho9">+ Carrinho</button>
+                <button class="excluir9">Excluir</button>
         </div>
     </section>
 </body>
